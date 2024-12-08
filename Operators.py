ecd618a38ffd522d682bbcd427bab0a14dc1b66f
Hitesh0210 <95519343+Hitesh0210@users.noreply.github.com>
2024-12-08 14:00:29 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,37 @@
+# Arithmatic operator -
+a=5
+b=2
+sum=a+b
+sub=a-b
+div=a/b
+mul=a*b
+mod=a%b #it gives remainder...
+print(sum)
+print(sub)
+print(div)
+print(mul)
+print(mod)
+
+# Relational Operator -
+a=60
+b=90
+print(a==b)
+print(a!=b)
+print(a<=b)
+print(a>=b)
+
+# Assignment operator -
+num=10
+num1=num+10
+num *=5;
+print(num1,num)
+
+# logical operator -
+a=10
+b=40
+print(not False)
+print(not (a>b))
+
+a=int(5.77)
+b=4
+print(a+b)
