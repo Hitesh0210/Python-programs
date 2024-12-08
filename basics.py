a228b867efefb34da588270d1d109a3401f0028b
Hitesh0210 <95519343+Hitesh0210@users.noreply.github.com>
2024-12-08 13:33:12 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,27 @@
+#Python Basics - 
+name = "hitesh"
+age = 22
+print("my name is :",name)
+age2=age
+print(age2)
+
+# in python there is no need to mention data types.
+x=20
+y=44.44
+name="hitesh"
+is_name= True
+z=-34
+f=None
+
+# type function
+print(type(is_name)) # type is used to show which data type we are using 
+print(type(name))
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(f)) 
+
+# input-
+age=int(input("age:"))
+name=input("name:")
+
